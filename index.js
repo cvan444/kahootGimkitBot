eaf44b90f1cb7676d0dd5da059e5cd9e4fb56988
cvan444 <127257962+cvan444@users.noreply.github.com>
2023-04-17 11:25:22 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs/promises')
+const cron = require('node-cron')
+
+
+//Edit these values 
+const kahootKey = ''
+const userName = ' '
+
+
+//start puppetteer instructions
+
+async function start() {
+    for (let i = 0; i < 10; i++) {
+    
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.goto("https://kahoot.it/");
+
+   
+   //Select kahoot input field 
+    const kahootInputField = '#game-input';
+    await page.waitForSelector(kahootInputField);
+    const field = await page.$(kahootInputField);
+
+    //Paste the kahoot key into the field
+    await field.type(kahootKey);
+
+
+    //Click submit button
+    const clickButtonSelector = '#root > div.controller__AppWrapper-sc-1m4rw0k-0.kJqUlD > div > div > div.sc-bATJPV.eBHtWy.sc-dzAwWg.drFGIL > div.sc-glalpP.duYZnJ > div.sc-bTtZEv.tzTnm > main > div > form > button';
+    await page.waitForSelector(clickButtonSelector);
+    await page.click(clickButtonSelector);
+
+
+    //Select name input field
+    const nameInputField = '#nickname';
+    await page.waitForSelector(nameInputField);
+    const nameField = await page.$(nameInputField);
+
+    //Paste the name into the field
+    const numberGenerator = Math.floor(Math.random() * 9000) + 1000; //generate string of nums to attach after username
+    await nameField.type(userName + numberGenerator.toString());
+
+
+    //Click OK, go! button
+    const clickNameSelector = '#root > div.controller__AppWrapper-sc-1m4rw0k-0.kJqUlD > div > div > div > div.sc-glalpP.duYZnJ > div.sc-bTtZEv.tzTnm > main > div > form > button';
+    await page.waitForSelector(clickNameSelector);
+    await page.click(clickNameSelector);
+
+
+    //Close browser after 1 seconds 
+    setTimeout(async () => {
+        await browser.close();
+      }, 1000);
+    
+    }
+}
+
+start()
